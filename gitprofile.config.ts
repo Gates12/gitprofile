1406807a4d54177e3a478ab902cb58bcf6ef1477
Gates Babu <87831348+Gates12@users.noreply.github.com>
2024-09-12 21:26:40 +0530

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -3,18 +3,13 @@
 const CONFIG = {
   github: {
     username: 'Gates12', // Your GitHub org/user name. (This is the only required config)
-    ortBy: 'stars', // stars | updated
+    sortBy: 'stars', // stars | updated
     limit: 6, // How many projects to display.
     exclude: {
-      forks: false, // Forked projects will not be displayed if set to true.
-      projects: ['ATM-SIMULATOR','gitprofile','JAVA-PROJECT','Movie-Ticket-Booking-system'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+      forks: true, // Forked projects will not be displayed
+      projects: ['ATM-SIMULATOR', 'JAVA-PROJECT', 'Movie-Ticket-Booking-system'], // Excluded non-forked projects
     },
   },
-  /**
-   * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
-   * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
-   * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
-   */
   base: '/gitprofile/',
   projects: {
     github: {
@@ -25,9 +20,10 @@ const CONFIG = {
         sortBy: 'stars', // Sort projects by 'stars' or 'updated'
         limit: 8, // How many projects to display.
         exclude: {
-          forks: false, // Forked projects will not be displayed if set to true.
-          projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
+          forks: true, // Forked projects will not be displayed
+          projects: ['ATM-SIMULATOR', 'JAVA-PROJECT', 'Movie-Ticket-Booking-system'], // Excluded non-forked projects
         },
+      },
       },
       manual: {
         // Properties for manually specifying projects
